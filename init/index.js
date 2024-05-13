1c29d4b2d00878cfa0c6d552991edfc20f8b7bdc
syedNaveedullah <snaveedliet@gmail.com>
2024-05-13 16:27:44 +0530

    initialize data in DB

---
+++ init/index.js
@@ -0,0 +1,30 @@
+//requring mongoose 
+const mongoose = require("mongoose");
+//requring data for initializing from data.js file 
+const initdata = require("./data.js");
+//requring modele named Listing in listing.js file 
+const Listing = require("../models/listing.js"); 
+
+//connecting to data base 
+const MONGO_URL = "mongodb://127.0.0.1:27017/travelingAgent";
+main().then(res=> {
+    console.log("connected to DB");
+}).catch(err => console.log(err));
+
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+
+//function to insert data 
+const initDB = async ()=>{
+    //empty the random data from DB
+    await Listing.deleteMany({});
+    //inserting data 
+    await Listing.insertMany(initdata.data);
+    console.log("data was initialize");
+}
+
+//calling the initdata function
+initDB();
